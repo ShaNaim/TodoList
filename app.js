0119f1bf16fc153176f895f0fcfc0a1164523750
ShaNaim <shanaim2k15@gmail.com>
2021-02-19 13:17:12 +0600

    initial setup

--- app.js
+++ app.js
@@ -1,10 +1,30 @@
+const { render } = require("ejs");
 const express = require("express");
-
+const mongoose = require("mongoose");
 const app = express();
 
-app.set("view engine", "ejs");
-
+// middlewares
 app.use(express.static("public"));
 
-app.listen(5000);
-console.log("listing to port: 5000");
+// Bootstrap
+// app.use("/css", express.static("./node_modules/bootstrap/dist/css"));
+// app.use("/js", express.static("./node_modules/bootstrap/dist/js"));
+// app.use("/js", express.static("./node_modules/jquery/dist"));
+
+// view engine
+app.set("view engine", "ejs");
+
+//listing for requiest
+app.listen(3000);
+
+//DB connection
+const dbURI = "";
+mongoose.connect(dbURI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  use,
+});
+//Routes
+app.get("/", (req, res) => {
+  res.render("home");
+});
